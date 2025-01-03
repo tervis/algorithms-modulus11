7210785d8a1d0f207815b7948427615ec3c15d4f
Mika Tervonen <mtervonen80@gmail.com>
2025-01-03 09:22:10 +0200

    update

--- src/Modulus11.php
+++ src/Modulus11.php
@@ -8,7 +8,15 @@ namespace Tervis\Algorithms;
  */
 class Modulus11
 {
-    private const FACTORS = [2,3,4,5,6,7];
+    private const FACTORS = [2, 3, 4, 5, 6, 7];
+
+    public static function validate(int|string $number, ?array $factors = null): bool
+    {
+        $number = (string)$number;
+        list($partialNumber, $checkDigit) = str_split($number, strlen($number) - 1);
+
+        return static::getChecksum($partialNumber, $factors) == $checkDigit;
+    }
 
     /**
      * @param int|string $number
@@ -17,13 +25,14 @@ class Modulus11
      */
     private static function getChecksum(int|string $number, ?array $factors = null): string
     {
-        $factors  = $factors ?: self::FACTORS;
-        $number   = preg_replace('/[^\wX]/','',strtoupper((string)$number));
-        $sequence = array_reverse(str_split($number));
-        $sum      = 0;
-        for($i=0; $i<count($sequence); $i++){
-            $factor = $factors[$i%count($factors)];
-            $sum    += ($sequence[$i] * $factor);
+        $factors = $factors ? array_map('intval', $factors) : self::FACTORS;
+        $number = preg_replace('/[^\wX]/', '', strtoupper((string)$number));
+        $parts = array_reverse(str_split($number));
+        $parts = array_map('intval', $parts);
+        $sum = 0;
+        for ($i = 0; $i < count($parts); $i++) {
+            $factor = $factors[$i % count($factors)];
+            $sum += ($parts[$i] * $factor);
         }
 
         $calculus = 11 - $sum % 11;
@@ -34,21 +43,13 @@ class Modulus11
         };
     }
 
-    public static function validate(int|string $number, ?array $factors = null): bool
-    {
-        $number = (string)$number;
-        list($partialNumber,$checkDigit) = str_split($number, strlen($number)-1);
-
-        return static::getChecksum($partialNumber,$factors) == $checkDigit;
-    }
-
     public static function calculate(int|string $partial_number, ?array $factors = null): string
     {
-        return static::getChecksum($partial_number,$factors);
+        return static::getChecksum($partial_number, $factors);
     }
 
     public static function create($partial_number, ?array $factors = null): string
     {
-        return $partial_number . static::getChecksum($partial_number,$factors);
+        return $partial_number . static::getChecksum($partial_number, $factors);
     }
 }
--- tests/Modulus11Test.php
+++ tests/Modulus11Test.php
@@ -4,11 +4,49 @@ declare(strict_types=1);
 namespace Tervis\Algorithms\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Tervis\Algorithms\Modulus11;
 
 /**
  * @author Mika Tervonen <mtervonen80@gmail.com>
  */
 class Modulus11Test extends TestCase
 {
+    protected $withCheckDigit1 = '1234.56.78903';
+    protected $withoutCheckDigit1 = '1234.56.7890';
+    protected $theCheckDigit1 = '3';
 
+    protected $withCheckDigit2 = '036522X';
+    protected $withoutCheckDigit2 = '036522';
+    protected $theCheckDigit2 = 'X';
+
+    public function testValidate()
+    {
+        $this->assertTrue(Modulus11::validate($this->withCheckDigit1));
+        $this->assertTrue(Modulus11::validate($this->withCheckDigit2));
+    }
+
+    public function testCalculate()
+    {
+        $this->assertSame($this->theCheckDigit1, Modulus11::calculate($this->withoutCheckDigit1));
+        $this->assertSame($this->theCheckDigit2, Modulus11::calculate($this->withoutCheckDigit2));
+    }
+
+    public function testAppendCheckDigit()
+    {
+        $appended = Modulus11::create($this->withoutCheckDigit1);
+        $this->assertSame($this->withoutCheckDigit1 . $this->theCheckDigit1, $appended);
+        $this->assertTrue(Modulus11::validate($appended));
+
+        $appended = Modulus11::create($this->withoutCheckDigit2);
+        $this->assertSame($this->withoutCheckDigit2 . $this->theCheckDigit2, $appended);
+        $this->assertTrue(Modulus11::validate($appended));
+    }
+
+    public function testCustomFactors()
+    {
+        $this->assertTrue(Modulus11::validate('11223346'));
+        $this->assertTrue(Modulus11::validate('1705833214', [2, 5, 4, 9, 8, 1, 6, 7, 3]));
+        $this->assertTrue(Modulus11::validate('1705833214', ['2', '5', '4', '9', '8', '1', '6', '7', '3']));
+        $this->assertSame('3', Modulus11::calculate('1705833214'));
+    }
 }
